fab146affcf0df9588c1aafa460181d4f746aa18
Andrew Murray <radarhere@users.noreply.github.com>
2024-01-18 20:33:14 +1100

    Added type hints

--- client/test_image_results/client.py
+++ client/test_image_results/client.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+from typing import Any
 
 import requests
 import os
@@ -10,29 +12,29 @@ API_KEY = os.environ.get('PILLOW_TEST_API_KEY', None)
 # suppress requests logging info
 logging.getLogger('requests').setLevel(logging.ERROR)
 
-def _send_one(presigned_struct, img):
+def _send_one(presigned_struct: dict[str, Any], img) -> None:
     requests.post(presigned_struct['url'],
                   data=presigned_struct['fields'],
                   files={'file':img}).raise_for_status()
 
-def _convert(img):
+def _convert(img) -> bytes:
     out = io.BytesIO()
     img.save(out, format='PNG')
     return out.getvalue()
 
-def _geturls():
+def _geturls() -> Any:
     req = requests.get(API+'/upload', headers={'x-api-key': API_KEY})
     req.raise_for_status()
     return req.json()
 
-def upload(result, target):
-    if not API_KEY: return
+def upload(result, target) -> str | None:
+    if not API_KEY:
+        return None
 
     urls = _geturls()
 
     _send_one(urls['upload']['result'], _convert(result))
     _send_one(urls['upload']['target'], _convert(target))
 
-    return API + urls['display']
-                        
-
+    display: str = urls['display']
+    return API + display
